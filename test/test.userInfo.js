1175920c650679574de58791e9bec6f196a0589a
cairumeng <cairumeng@gmail.com>
2020-02-22 19:14:25 +0100

    feat:个人主页单元测”

--- test/test.userInfo.js
+++ test/test.userInfo.js
@@ -3,9 +3,10 @@
  * @author  rumengbaobao
  */
 
- //cookie 是用户的敏感信息，这里只能是用测试用户的cookie
- //每次测试用户登录都需要更新这里的cookie
- 
- module.exports={
- COOKIE = 'weibo.sid=PbSvGVQndOY2DCCNlv8_JN6pQG2b07vM; weibo.sid.sig=UUdIreTb158NAPA03fFwTihXsSU'
- }
+//cookie 是用户的敏感信息，这里只能是用测试用户的cookie
+//每次测试用户登录都需要更新这里的cookie
+
+module.exports = {
+    USER_NAME : 'lisi',
+    COOKIE : 'weibo.sid=JKAT2ig3bTrWWnlXc2UPAfZk7iBx0Knb; weibo.sid.sig=0NFd5K8EjLO3O8XckyWx3T1t9vg'
+}
