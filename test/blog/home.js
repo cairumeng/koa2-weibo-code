59c47548b0270a396998a301b463a60bd20a889d
cairumeng <cairumeng@gmail.com>
2020-02-22 11:31:53 +0100

    微博id更新

--- test/blog/home.js
+++ test/blog/home.js
@@ -7,6 +7,10 @@
 
 const {COOKIE} = require('../test.userInfo')
 const server = require('../server')
+
+//存储微博id
+let BLOG_ID = ''
+
 test('创建微博应该成功', async () => {
     //定义测试内容
     const content = '单元测试自动创建的微博_' + Date.now()
@@ -24,5 +28,7 @@ test('创建微博应该成功', async () => {
     expect(res.body.errno).toBe(0)
     expect(res.body.data.content).toBe(content)
     expect(res.body.data.image).toBe(image)
+
+    BLOG_ID = res.body.data.id
 })
 
